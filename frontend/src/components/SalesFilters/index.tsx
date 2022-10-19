cc4d25af1c42ebd064b7bca0222122cf706ce69d
wfilho91 <wellington79620@gmail.com>
2022-10-19 11:04:21 -0300

    add state management with useState in SalesFilters

--- frontend/src/components/SalesFilters/index.tsx
+++ frontend/src/components/SalesFilters/index.tsx
@@ -1,28 +1,33 @@
+import { useState } from 'react'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
 import './styles.css'
 
 export function SalesFilters() {
+  const min = new Date(new Date().setDate(new Date().getDate() - 365))
+  const [minDate, setMinDate] = useState(min)
+  const [maxDate, setMaxDate] = useState(new Date())
+
   return (
     <>
       <h2 className="dsmeta-sales-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-container">
           <DatePicker
-            selected={new Date()}
-            onChange={(date: Date) => {}}
+            selected={minDate}
+            onChange={(date: Date) => setMinDate(date)}
             className="dsmeta-form-control"
             dateFormat="dd/MM/yyyy"
           />
         </div>
         <div className="dsmeta-form-control-container">
-        <DatePicker
-    selected={new Date()}
-    onChange={(date: Date) => {}}
-    className="dsmeta-form-control"
-    dateFormat="dd/MM/yyyy"
-/>
+          <DatePicker
+            selected={maxDate}
+            onChange={(date: Date) => setMaxDate(date)}
+            className="dsmeta-form-control"
+            dateFormat="dd/MM/yyyy"
+          />
         </div>
       </div>
     </>
