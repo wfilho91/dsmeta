42a3725efca8f008443c951c9172b9d575302c9a
wfilho91 <wellington79620@gmail.com>
2022-10-19 16:58:23 -0300

    query by implemented date

--- backend/src/main/java/com/devsuperior/dsmeta/resources/SaleResource.java
+++ backend/src/main/java/com/devsuperior/dsmeta/resources/SaleResource.java
@@ -1,25 +1,28 @@
 package com.devsuperior.dsmeta.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
 import com.devsuperior.dsmeta.services.SaleService;
 
 @RestController
-@RequestMapping(value="/sales")
+@RequestMapping(value = "/sales")
 public class SaleResource {
+
 	@Autowired
 	private SaleService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<Sale>>findAllSales(){
-		List<Sale> sales = service.findAllSales();
+	public ResponseEntity<Page<Sale>> findAll(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate, Pageable pageable) {
+		Page<Sale> sales = service.findSales(minDate,maxDate, pageable);
 		return ResponseEntity.ok().body(sales);
 	}
 }
